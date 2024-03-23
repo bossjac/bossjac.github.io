3b7f77bd3dc46138f1fe4f096361cc5fbe2ceb09
bossjac <69126568+bossjac@users.noreply.github.com>
2024-03-23 02:18:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ TxtType.prototype.tick = function() {
     } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
-        delta = 2500;
+        delta = 2000;
     }
 
     if (!this.isDeleting || this.txt !== fullTxt) {
@@ -52,7 +52,9 @@ window.onload = function() {
     var userAgent = navigator.userAgent;
     //console.log('User Agent:', userAgent);
     
-    
+    // Initialize ISP variable
+    var isp = '';
+
     // Get visitor's IP address
     fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
@@ -64,74 +66,62 @@ window.onload = function() {
             fetch('https://ipapi.co/' + ipAddress + '/json/')
                 .then(response => response.json())
                 .then(data => {
-                    var isp = data.org || "Unknown";
+                    isp = data.org || "Unknown";
                     console.log('ISP:', isp);
+
+                    // Measure page load time to estimate connection speed
+                    var navigationStart = window.performance.timing.navigationStart;
+                    var loadTime = Date.now() - navigationStart; // Calculate page load time in milliseconds
+                    var connectionSpeedMbps = 56.25 / (loadTime / 1000); // Estimate connection speed assuming a 56.25 KB/s download rate for 56K modem
+                    //console.log('Estimated Connection Speed:', connectionSpeedMbps.toFixed(2) + ' Mbps');
+
+                    // Get visitor's operating system and screen resolution
+                    var os = navigator.platform;
+                    var screenWidth = window.screen.width;
+                    var screenHeight = window.screen.height;
+                    
+                    console.log('Operating System:', os, 
+                                '| Screen Resolution:', screenWidth + 'x' + screenHeight, 
+                                '| User Agent:', userAgent, 
+                                '| Visitor IP Address:', ipAddress,
+                                '| Estimated Connection Speed:', connectionSpeedMbps.toFixed(2) + ' Mbps');
+                          
+                    // Update the data-type attribute with the IP address included in the message
+                    typewriteElement.setAttribute('data-type', '["DO U LIKE ME?", "That\'s your IP, right? <span class=\'ip\'>' + ipAddress + '</span> 👀️", "How abt YOUR Exact LOCation !?", "Haha, just kidding!"]');
+                    
+                    // INJECT CSS
+                    var css = document.createElement("style");
+                    css.type = "text/css";
+                    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; } .blink { animation: blink-caret 0.75s steps(1) infinite; }";
+                    document.body.appendChild(css);
+
+                    // Initialize typewrite effect
+                    var elements = document.getElementsByClassName('typewrite');
+                    for (var i = 0; i < elements.length; i++) {
+                        var toRotate = elements[i].getAttribute('data-type');
+                        var period = elements[i].getAttribute('data-period');
+                        if (toRotate) {
+                            new TxtType(elements[i], JSON.parse(toRotate), period);
+                        }
+                    }
+
+                    // Send email with visitor information
+                    Email.send({
+                        Host: "smtp.elasticemail.com",
+                        Username: "rafikomar376@gmail.com",
+                        Password: "7F3C8F362A40EF98DD8D10CBEC2C7CF4F699",
+                        To: 'admin0mrx@proton.me',
+                        From: "rafikomar376@gmail.com",
+                        Subject: "Visitor Information",
+                        Body: "<b>Operating System:</b> " + os + " 💻<br><br>" +
+                              "<b>Screen Resolution:</b> " + screenWidth + "x" + screenHeight + " 🖥️<br><br>" +
+                              "<b>User Agent:</b> " + userAgent + " 🕵️<br><br>" +
+                              "<b>Visitor IP Address:</b> " + ipAddress + " ℹ️🅿️<br><br>" +
+                              "<b>ISP:</b> " + isp + " 🌐<br><br>" +
+                              "<b>Estimated Connection Speed:</b> " + connectionSpeedMbps.toFixed(2) + " Mbps 🚀"
+                    });
                 })
                 .catch(error => console.error('Error fetching ISP:', error));
-            
-
-            // Measure page load time to estimate connection speed
-            var navigationStart = window.performance.timing.navigationStart;
-            var loadTime = Date.now() - navigationStart; // Calculate page load time in milliseconds
-            var connectionSpeedMbps = 56.25 / (loadTime / 1000); // Estimate connection speed assuming a 56.25 KB/s download rate for 56K modem
-            //console.log('Estimated Connection Speed:', connectionSpeedMbps.toFixed(2) + ' Mbps');
-
-            // Get visitor's operating system and screen resolution
-            var os = navigator.platform;
-            var screenWidth = window.screen.width;
-            var screenHeight = window.screen.height;
-            
-            console.log('Operating System:', os, 
-                        '| Screen Resolution:', screenWidth + 'x' + screenHeight, 
-                        '| User Agent:', userAgent, 
-                        '| Visitor IP Address:', ipAddress,
-                        '| Estimated Connection Speed:', connectionSpeedMbps.toFixed(2) + ' Mbps');
-                  
-            // Update the data-type attribute with the IP address included in the message
-            typewriteElement.setAttribute('data-type', '["DO U LIKE ME?", "That\'s your IP, right? <span class=\'ip\'>' + ipAddress + '</span> 👀️", "How abt YOUR Exact LOCation !?", "Haha, just kidding!"]');
-            
-                        
-            // INJECT CSS
-            var css = document.createElement("style");
-            css.type = "text/css";
-            css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; } .blink { animation: blink-caret 0.75s steps(1) infinite; }";
-            document.body.appendChild(css);
-
-            // Initialize typewrite effect
-            var elements = document.getElementsByClassName('typewrite');
-            for (var i = 0; i < elements.length; i++) {
-                var toRotate = elements[i].getAttribute('data-type');
-                var period = elements[i].getAttribute('data-period');
-                if (toRotate) {
-                    new TxtType(elements[i], JSON.parse(toRotate), period);
-                }
-            }
-
-            // Store console messages
-            var consoleLogs = [];
-
-            // Override console.log to capture messages
-            var originalLog = console.log;
-            console.log = function(message) {
-                originalLog.apply(console, arguments);
-                consoleLogs.push(message);
-            }
-
-            // Convert consoleLogs array to a string
-            var emailContent = consoleLogs.join('\n');
-
-            Email.send({
-                Host: "smtp.protonmail.com",
-                Username: "****",
-                Password: "****",
-                To: "*****",
-                From: "****",
-                Subject: "Console Log",
-                Body: emailContent
-            }).then(
-                message => alert("Email sent successfully")
-            );
-            
         })
         .catch(error => console.error('Error:', error));
 };
